9f47783285aa599b22360d5a735960b64c4e7f84
github username <github email address>
2020-02-05 12:10:02 -0800

    mvp done

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,6 +9,9 @@ class Api::V1::UsersController < ApplicationController
     novel = user.novels.find_by(title: params[:novel])
     chapters = novel.chapters
 
+
+ colors = ['#b52b65', '#bd3a67', '#c44768', '#cc546a', '#d35f6b', '#db6b6d', '#e2766e', '#e9826f', '#f18d70', '#f89871', '#df5d62', '#d15461', '#c2e8ce', '#f2eee5', '#f6ad7b', '#be7575', '#dcffcc', '#9fdfcd', '#baabda', '#d79abc']
+
   response = {
     :wordCount => 0,
     :longestWord => "",
@@ -22,36 +25,67 @@ class Api::V1::UsersController < ApplicationController
     
   wordList = response[:wordList]
   tempchapters = response[:wordList][:chapters]
-  tempwords = {}
-  tempwordsarr = []
+  templateObject = {
+    full: 0
+  }
 
+  tempwords = {}
 
   chapters.each_with_index do |item, index|
-      tempchapters.push({:label => item.title})
+    templateObject["#{item.title}"] = 0
+  end
+
+
+totalWords= 0
+wordLengthChecker = 0
+templongestWord = ""
+totalCharacters = 0
+
+  chapters.each_with_index do |item, index|
+      tempchapters.push({:label => item.title, :data => [], :backgroundColor => colors.pop(), :barThickness => 8})
+
       w = item[:content].split(' ')
       w.each do |w|
+        totalWords += 1
+        totalCharacters += w.length
+        if w.length > wordLengthChecker
+          wordLengthChecker = w.length
+          templongestWord = w 
+        end
 
         if tempwords[w.downcase]
-          tempwords[w.downcase] += 1
+          tempwords[w.downcase][:full] += 1
+          tempwords[w.downcase]["#{item.title}"] += 1
         else
-          tempwords[w.downcase] = 1
+          a = templateObject.dup
+          tempwords[w.downcase] = a
+          tempwords[w.downcase][:full] += 1
+          tempwords[w.downcase]["#{item.title}"] += 1
         end
       end
   end
 
-tempwords.each do |k, v|
-  tempwordsarr.push(k)
+tempwords = tempwords.sort_by {|_key, value| -value[:full]}.to_h
+
+
+counter = 0
+tempwords.each do |k,v|
+    response[:wordList][:words].push(k)
+    response[:wordList][:chapters].each do |item|
+      item[:data].push(v[item[:label]])
+      end
 end
 
 
 
-response[:wordList][:words] = tempwordsarr
+response[:wordCount] = totalWords
+response[:numberOfUniqueWords] = tempwords.length
+response[:longestWord] = templongestWord
+response[:avgWordLength] = totalCharacters / totalWords
 
-
-tempwords = tempwords.sort_by {|_key, value| -value}.to_h
   
 
-    render json: tempwords
+    render json: response
   end
 
 
