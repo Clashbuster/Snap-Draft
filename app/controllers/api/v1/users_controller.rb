4cc0675c8536a9e1ec908ec79e51ca7f67bceec6
github username <github email address>
2020-02-07 14:51:30 -0800

    updates

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,7 +10,9 @@ class Api::V1::UsersController < ApplicationController
     chapters = novel.chapters
 
 
- colors = ['#b52b65', '#bd3a67', '#c44768', '#cc546a', '#d35f6b', '#db6b6d', '#e2766e', '#e9826f', '#f18d70', '#f89871', '#df5d62', '#d15461', '#c2e8ce', '#f2eee5', '#f6ad7b', '#be7575', '#dcffcc', '#9fdfcd', '#baabda', '#d79abc']
+#  colors = ['#b52b65', '#bd3a67', '#c44768', '#cc546a', '#d35f6b', '#db6b6d', '#e2766e', '#e9826f', '#f18d70', '#f89871', '#df5d62', '#d15461', '#c2e8ce', '#f2eee5', '#f6ad7b', '#be7575', '#dcffcc', '#9fdfcd', '#baabda', '#d79abc']
+#  colors = ['#50d890', '#50cd9a', '#50c1a5', '#4fb5b0', '#4fa9ba', '#4f9dc6', '#4d92c2', '#4a89b5', '#4781a8', '#44789c', '#417090', '#3e6783', '#3b5f77', '#38566b', '#354e60', '#324654', '#2f3e49', '#2c363d', '#2a2f32', '#272727']
+colors = ['#272727', '#2a2f32', '#2c363d', '#2f3e49', '#324654', '#354e60', '#38566b', '#3b5f77', '#3e6783', '#417090', '#44789c', '#4781a8', '#4a89b5', '#4d92c2', '#4f9dc6', '#4fa9ba', '#4fb5b0', '#50c1a5', '#50cd9a', '#50d890']
 
   response = {
     :wordCount => 0,
@@ -42,12 +44,13 @@ templongestWord = ""
 totalCharacters = 0
 
   chapters.each_with_index do |item, index|
-      tempchapters.push({:label => item.title, :data => [], :backgroundColor => colors.pop(), :barThickness => 8})
+      tempchapters.push({:label => item.title, :data => [], :backgroundColor => colors.pop(), :barThickness => 17})
 
       w = item[:content].split(' ')
       w.each do |w|
         totalWords += 1
         totalCharacters += w.length
+        w.gsub!(/[^0-9A-Za-z]/, '')
         if w.length > wordLengthChecker
           wordLengthChecker = w.length
           templongestWord = w 
@@ -66,20 +69,41 @@ totalCharacters = 0
   end
 
 tempwords = tempwords.sort_by {|_key, value| -value[:full]}.to_h
+response[:numberOfUniqueWords] = tempwords.length
 
 
-counter = 0
+top100Words = ["a","all","about","also","and","as","at","be","because","by","can","come","could","day","do","even","find","first","for","from","get","give","go
+","have","he","her","here","him","how","I","if","in","into","it","it","just","know","like","look","make","man","many","me","more","my
+","new","no","not","now","of","on","one","only","or","other","our","out","people","say","see","she","so","some","take","tell","than","that
+","the","their","them","then","there","they","thing","think","this","those","time","to","two","up","use","very","want","way","we","well
+","what","when","which","who","will","with","would","year","you","your"]
+
+
+top100Words.each do |item|
+
+if tempwords["#{item}"]
+tempwords.delete(item)
+  end
+end
+# puts tempwords
+
+keycounter = 0
 tempwords.each do |k,v|
+
+ break if keycounter === 65
+
+
     response[:wordList][:words].push(k)
     response[:wordList][:chapters].each do |item|
       item[:data].push(v[item[:label]])
       end
+
+      keycounter += 1
 end
 
 
 
 response[:wordCount] = totalWords
-response[:numberOfUniqueWords] = tempwords.length
 response[:longestWord] = templongestWord
 
 if totalWords != 0
@@ -119,11 +143,15 @@ end
 
   def sprint
     user = User.find_by(username: params[:user])
-    puts user
+    # puts user
     novel = user.novels.find_by(title: params[:novel])
     
     if params[:chapter] != ""
       chapter = novel.chapters.create(title: params[:chapter], content: params[:text])
+      chapter.save
+    elsif novel.chapters.last == nil
+      chapter = novel.chapters.create(title: "Chapter 1")
+      chapter.content = params[:text]
       chapter.save
     else
       chapter = novel.chapters.last
