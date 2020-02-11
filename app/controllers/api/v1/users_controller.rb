4295bc819f046a45063905b6f91c85f8fd2916e6
github username <github email address>
2020-02-11 09:41:48 -0800

    deploy

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -195,11 +195,8 @@ end
     decodedtoken = decoded_token()
     user = User.find_by(id: decodedtoken[0]["user_id"])
 
-    if user.novels
-      response = user.novels
-    else
-      response = {error: "no novels"}
-    end
+    response = user.novels
+    
 
 
 
